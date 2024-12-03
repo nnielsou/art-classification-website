b49f270fb7ddc822abba9a4758ad64dc875a1649
Niels Nizard <182967128+nnielsou@users.noreply.github.com>
2024-12-03 12:53:38 +0100

    Art classification website

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+'''
+Art Classification front
+'''
+
+
+
+
+
+#API CALL
+url = #API URL
+'''
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+
+'''
